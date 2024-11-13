56c16ca4f0e22af827fc7be4e50dbc61febd38b0
JosephLiao542211 <billan452211@gmail.com>
2024-11-12 23:46:35 -0500

    testing coverage fix

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  transform: {
-    "^.+\\.jsx?$": "babel-jest"
-  }
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/pages/**/*.{js,jsx}',  // Collect coverage for all JS/JSX files in the `pages` folder
+    '!src/pages/**/*.test.{js,jsx}',  // Exclude test files from coverage
+  ],
+  coverageDirectory: 'coverage',  // Directory to store coverage reports
+  coverageReporters: ['text', 'lcov'],  // Coverage report format
 };
