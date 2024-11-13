4f9cae0525ed1c7f253f033655353ad04483ee94
JosephLiao542211 <billan452211@gmail.com>
2024-11-12 23:59:30 -0500

    something

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  collectCoverage: true,
-  collectCoverageFrom: [
-    'src/pages/**/*.{js,jsx}',  // Collect coverage for all JS/JSX files in the `pages` folder
-    '!src/pages/**/*.test.{js,jsx}',  // Exclude test files from coverage
-  ],
-  coverageDirectory: 'coverage',  // Directory to store coverage reports
-  coverageReporters: ['text', 'lcov'],  // Coverage report format
+  transform: {
+    "^.+\\.jsx?$": "babel-jest"
+  }
 };
