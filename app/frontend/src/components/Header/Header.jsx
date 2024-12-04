7ed9d4f991c50845c7d4a052d2128c101fb2609d
Joseph Liao <100002111+JosephLiao542211@users.noreply.github.com>
2024-12-03 23:25:01 -0500

    E2E works up to registration

--- app/frontend/src/components/Header/Header.jsx
+++ app/frontend/src/components/Header/Header.jsx
@@ -1,9 +1,8 @@
 import React, { useContext } from 'react';
 import './Header.css';
-import { LoginContext } from '../../pages/Landing/Landing';// Make sure to import your LoginContext
+import { LoginContext } from '../../pages/Landing/Landing'; // Make sure to import your LoginContext
 
 const Header = () => {
-    
     const { userData } = useContext(LoginContext);
 
     const navigateToSignup = () => {
@@ -13,26 +12,49 @@ const Header = () => {
         window.location.href = '/profile/' + userData._id;
     };
 
-
     return (
         <header className="header">
-    
-            <img src="logo.png" alt="Logo" style={{ marginBottom: '0px', height: '60px', cursor: 'pointer' }} onClick={() => window.location.href = '/'} />
-            
+            <img
+                src="logo.png"
+                alt="Logo"
+                style={{
+                    marginBottom: '0px',
+                    height: '60px',
+                    cursor: 'pointer',
+                }}
+                onClick={() => (window.location.href = '/')}
+            />
+
             <nav className="nav">
-                <a href="/" className="nav-item">Home</a>
-                <a href="#bookings" className="nav-item">Bookings</a>
-                <a href="explore" className="nav-item">Explore</a>
+                <a href="/" className="nav-item">
+                    Home
+                </a>
+                <a href="#bookings" className="nav-item">
+                    Bookings
+                </a>
+                <a href="explore" className="nav-item">
+                    Explore
+                </a>
 
                 {userData ? (
                     // If userData exists, show the user's name and a profile icon button
-                    <button className="nav-login-button" onClick={navigateTohome}>
-                        <img src="profile-icon.png" alt="Profile Icon" style={{ height: '50px', marginRight: '10px' }} />
+                    <button
+                        className="nav-login-button"
+                        onClick={navigateTohome}
+                    >
+                        <img
+                            src="profile-icon.png"
+                            alt="Profile Icon"
+                            style={{ height: '50px', marginRight: '10px' }}
+                        />
                         {userData.firstName}
                     </button>
                 ) : (
                     // If userData does not exist, show the login/signup button
-                    <button className="nav-login-button" onClick={navigateToSignup}>
+                    <button
+                        className="nav-login-button"
+                        onClick={navigateToSignup}
+                    >
                         Login/Signup
                     </button>
                 )}
